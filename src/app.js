ea695882eec96029cc27bc96951d3ce55faf5f32
MKBSP <mads@smsmig.dk>
2023-11-15 20:39:23 +0000

    This time I think it works, finished, and with the button

--- src/app.js
+++ src/app.js
@@ -7,5 +7,45 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  // Call generateExcuse once on page load
+  generateExcuse();
+
+  // Attach event listener to button
+  document
+    .querySelector("#generate-excuse")
+    .addEventListener("click", generateExcuse);
 };
+
+//Excude Function
+function generateExcuse() {
+  //excuse arrays
+  let who = ["The dog", "My grandma", "His turtle", "My bird", "The Dinosaur"];
+  let action = ["ate", "peed on", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //All the different parts of the excuse
+  let whoRandomNumber = Math.floor(Math.random() * who.length);
+  let excuseWho = who[whoRandomNumber];
+
+  let actionRandomNumber = Math.floor(Math.random() * action.length);
+  let excuseAction = action[actionRandomNumber];
+
+  let whatRandomNumber = Math.floor(Math.random() * what.length);
+  let excuseWhat = what[whatRandomNumber];
+
+  let whenRandomNumber = Math.floor(Math.random() * when.length);
+  let excuseWhen = when[whenRandomNumber];
+
+  let excuse =
+    excuseWho + " " + excuseAction + " " + excuseWhat + " " + excuseWhen;
+
+  document.querySelector("#my-excuse").innerHTML = excuse;
+}
